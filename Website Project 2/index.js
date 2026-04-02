cdfbcb066ae7b4b95136ce8666acc912cf0dd0f6
vivekp-anvitechsys-maker <vivekp.anvitechsys@gmail.com>
2026-04-02 17:27:43 +0530

    Add files via upload

---
+++ Website Project 2/index.js
@@ -0,0 +1,63 @@
+  document.body.classList.add("dark-mode");
+   
+    function toggleDarkMode()
+    {
+      document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            
+            toggleBtn.textContent = "☀️";
+        } else {
+            
+            toggleBtn.textContent = "🌙";
+        }
+    
+    }
+
+      // Toggle menu visibility on small screens
+    function toggleMenu() {
+        const links = document.querySelector('.navigation-links');
+        links.classList.toggle('active');
+        links.style.backgroundColor = '#0A0F1C';
+        links.style.padding = '10px';
+        links.classList.toggle('give-margin');
+    }
+
+function loadNavbar()
+{
+// Load navbar.html into the #navbar div
+fetch("./navbar.html")
+  .then(response => {
+    if (!response.ok) throw new Error("Navbar file not found");
+    return response.text();
+  })
+  .then(data => document.getElementById("navbar-element").innerHTML = data)
+  .catch(err => console.error(err));
+}   
+    
+
+function loadFooter()
+{
+
+
+  // Load footer.html into the footer div
+fetch("./footer.html")
+  .then(response => {
+    if (!response.ok) throw new Error("Footer file not found");
+    return response.text();
+  })
+  .then(data => document.getElementById("footer-element").innerHTML = data)
+  .catch(err => console.error(err));
+}
+
+function loadFaq()
+{
+  // Load footer.html into the footer div
+fetch("./faq.html")
+  .then(response => {
+    if (!response.ok) throw new Error("FAQ file not found");
+    return response.text();
+  })
+  .then(data => document.getElementById("faq-element").innerHTML = data)
+  .catch(err => console.error(err));
+}
