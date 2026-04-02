96e547f1ee87d90d33309e791a96675e5584fe87
vivekp-anvitechsys-maker <vivekp.anvitechsys@gmail.com>
2026-04-02 20:13:32 +0530

    Formatted document

--- Website Project 2/index.js
+++ Website Project 2/index.js
@@ -1,32 +1,31 @@
-  document.body.classList.add("dark-mode");
-   
-    function toggleDarkMode()
-    {
-      document.body.classList.toggle("dark-mode");
+document.body.classList.add("dark-mode");
+function toggleDarkMode()
+{
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) 
+    {     
+        toggleBtn.textContent = "☀️";
+    } 
+    else 
+    {  
+        toggleBtn.textContent = "🌙";
+    } 
+}
 
-        if (document.body.classList.contains("dark-mode")) {
-            
-            toggleBtn.textContent = "☀️";
-        } else {
-            
-            toggleBtn.textContent = "🌙";
-        }
-    
-    }
+ // Toggle menu visibility on small screens
+function toggleMenu() 
+{
+  const links = document.querySelector('.navigation-links');
+  links.classList.toggle('active');
+  links.style.backgroundColor = '#0A0F1C';
+  links.style.padding = '10px';
+  links.classList.toggle('give-margin');
+}
 
-      // Toggle menu visibility on small screens
-    function toggleMenu() {
-        const links = document.querySelector('.navigation-links');
-        links.classList.toggle('active');
-        links.style.backgroundColor = '#0A0F1C';
-        links.style.padding = '10px';
-        links.classList.toggle('give-margin');
-    }
-
+// Load navbar.html into the #navbar div
 function loadNavbar()
 {
-// Load navbar.html into the #navbar div
-fetch("./navbar.html")
+  fetch("./navbar.html")
   .then(response => {
     if (!response.ok) throw new Error("Navbar file not found");
     return response.text();
@@ -34,14 +33,11 @@ fetch("./navbar.html")
   .then(data => document.getElementById("navbar-element").innerHTML = data)
   .catch(err => console.error(err));
 }   
-    
 
+// Load footer.html into the footer div
 function loadFooter()
 {
-
-
-  // Load footer.html into the footer div
-fetch("./footer.html")
+  fetch("./footer.html")
   .then(response => {
     if (!response.ok) throw new Error("Footer file not found");
     return response.text();
@@ -50,10 +46,11 @@ fetch("./footer.html")
   .catch(err => console.error(err));
 }
 
+// Load faq.html into the footer div
 function loadFaq()
 {
   // Load footer.html into the footer div
-fetch("./faq.html")
+  fetch("./faq.html")
   .then(response => {
     if (!response.ok) throw new Error("FAQ file not found");
     return response.text();
